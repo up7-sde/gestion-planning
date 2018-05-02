c8a9f466363aa4eca8ca06713de5bfb650bb55af
pise <pise@ubuntu.ubuntu-domain>
2018-05-02 21:03:02 +0200

    Corrige procedure insererCours

--- bdd/tester_procedures.sql
+++ bdd/tester_procedures.sql
@@ -1,10 +1,12 @@
 -- petit script pour faire des tests pendant l'ecriture des procédures
 CALL InsererEnseignant("Jean", "Louis", 1);
 CALL InsererEnseignement("54AEe3EC", "Introduction à la biogeographie", 26, 18, 5, 1);
-CALL InsererCours(1, 2, 2018, "54AEe3EC", 18);
+-- 2016 devra s'afficher pas 2015 (a cause du nombmre d'heure)
+CALL InsererCours(1, 1, 2015, "54AEE1EC", 320);
+CALL InsererCours(1, 1, 2018, "54AEE1EC", 7);
 CALL InsererFormation("EDMR", 1);
 CALL ModifierEnseignant(1, "Jean", "DarmanGeaT", 1);
-CALL ModifierCours(1, 1, 1, 2018, "54AEE1EC", 15);
+-- CALL ModifierCours(1, 1, 1, 2016, "54AEE1EC", 15);
 CALL ModifierEnseignement("43IF5073", "BLOB", "Un cours chouette sur les blobs", 12, 13, 5, 3);
 CALL ModifierFormation("PISE", "PAAASE", 2);
 CALL SupprimerCours(2);
