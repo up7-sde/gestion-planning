8957ff9b5de7f70e29a928c623b6211e142a841a
remidlnn <remidlnn@gmail.com>
2018-05-02 21:34:43 +0200

    Factorise les procédures

--- bdd/tester_procedures.sql
+++ bdd/tester_procedures.sql
@@ -4,6 +4,12 @@ CALL InsererEnseignement("54AEe3EC", "Introduction à la biogeographie", 26, 18,
 -- 2016 devra s'afficher pas 2015 (a cause du nombmre d'heure)
 CALL InsererCours(1, 1, 2015, "54AEE1EC", 320);
 CALL InsererCours(1, 1, 2018, "54AEE1EC", 7);
+CALL InsererCours(1, 1, 2014, "54AEE1EC", 7);
+CALL InsererCours(1, 1, 2013, "54AEE1EC", 7);
+CALL InsererCours(1, 1, 2012, "54AEE1EC", 7);
+CALL InsererCours(1, 1, 2011, "54AEE1EC", 7);
+CALL InsererCours(1, 1, 2010, "54AEE1EC", 7);
+CALL InsererCours(1, 1, 2009, "54AEE1EC", 7);
 CALL InsererFormation("EDMR", 1);
 CALL ModifierEnseignant(1, "Jean", "DarmanGeaT", 1);
 -- CALL ModifierCours(1, 1, 1, 2016, "54AEE1EC", 15);
