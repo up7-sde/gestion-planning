f3395757c0206c1f394effc274f6823464a5b17c
remidlnn <remidlnn@gmail.com>
2018-05-13 11:34:58 +0200

    Echo dans le script de deploiement

--- mep/deployer.sh
+++ mep/deployer.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-# Se mettre dans le bon dossier
+# A lancer sur un serveur avec la commande :
+#ssh sde@ip 'bash -s' < deployer.sh
+
+echo "Se depalcer dans le bon dossier"
 cd ~/sites/sde
 
-# Sourcer les variable d'environnement
+echo "Sourcer et afficher les variables d'environnement"
 source mep/.env
+printenv
 
-# Supprimer tous les changements pour être certain de pouvoir faire le pull
+echo "Supprimer tous les changements pour être certain de pouvoir faire le pull"
 git checkout .
-# Obtenir les dernières sources de la branche master
+echo "Obtenir les dernières sources de la branche master"
 git pull origin master
 
-# Changer les mots de passe par les mots de passe issus des variables d'env
+echo "Changer les mots de passe par les mots de passe issus des variables d'env"
 sed -i -e "s/mdpadmin/$ADMIN_MYSQL_PASSWD/g" "$DIR_BDD/creer_bdd.sql"
 sed -i -e "s/mdpenseignant/$ENSEIGNANT_MYSQL_PASSWD/g" "$DIR_BDD/creer_bdd.sql"
 
-# Installer la base de données (a enlever quand la bdd sera finalisée)
+echo "Installer la base de données"
 cat "$DIR_BDD/creer_bdd.sql" "$DIR_BDD/donner_droit.sql" "$DIR_BDD/remplir_bdd.sql" | $MYSQL -u $ROOT_MYSQL_LOGIN -p$ROOT_MYSQL_PASSWD
