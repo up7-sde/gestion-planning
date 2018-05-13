435d667093ccbe8a79495b7386b287df4072fa85
remidlnn <remidlnn@gmail.com>
2018-05-13 11:24:02 +0200

    Améliore le script de deploiement

--- mep/deployer.sh
+++ mep/deployer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Se mettre dans le bon dossier (a décommenter en prod)
+# Se mettre dans le bon dossier
 cd ~/sites/sde
 
 # Sourcer les variable d'environnement
@@ -8,16 +8,12 @@ source mep/.env
 
 # Supprimer tous les changements pour être certain de pouvoir faire le pull
 git checkout .
-# Obtenir les dernières sources de la branche master (a décommenter en prod)
+# Obtenir les dernières sources de la branche master
 git pull origin master
 
-# Changer les mots de passe par les mots de passe issu des variables d'env
+# Changer les mots de passe par les mots de passe issus des variables d'env
 sed -i -e "s/mdpadmin/$ADMIN_MYSQL_PASSWD/g" "$DIR_BDD/creer_bdd.sql"
 sed -i -e "s/mdpenseignant/$ENSEIGNANT_MYSQL_PASSWD/g" "$DIR_BDD/creer_bdd.sql"
 
 # Installer la base de données (a enlever quand la bdd sera finalisée)
-cat "$DIR_BDD/creer_bdd.sql" "$DIR_BDD/donner_droit.sql" "$DIR_BDD/remplir_bdd.sql" > "$DIR_BDD/toto.sql"
-$MYSQL -u $ROOT_MYSQL_LOGIN -p$ROOT_MYSQL_PASSWD < "$DIR_BDD/toto.sql"
-
-# Supprimer le fichier temp toto
-rm "$DIR_BDD/toto.sql"
+cat "$DIR_BDD/creer_bdd.sql" "$DIR_BDD/donner_droit.sql" "$DIR_BDD/remplir_bdd.sql" | $MYSQL -u $ROOT_MYSQL_LOGIN -p$ROOT_MYSQL_PASSWD
